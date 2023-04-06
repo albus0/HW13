f4aa72dbaa5a6d4ff0b6c2ab562a4350d089b7a5
Albina Lutfullina <106741510+albinalutfullina@users.noreply.github.com>
2023-04-06 23:25:40 +0500

    Initial Commit

---
+++ HW13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13
+//
+//  Created by Альбина on 06.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
